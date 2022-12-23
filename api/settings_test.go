64f3716ea1665e7481506095fd2270353cbeb0ba
김상규 <david@cluedit.com>
2022-12-24 05:25:34 +0900

    feat: add example variables to example.env

--- api/settings_test.go
+++ api/settings_test.go
@@ -46,6 +46,8 @@ func TestSettings_DefaultProviders(t *testing.T) {
 	require.True(t, p.Twitch)
 	require.True(t, p.WorkOS)
 	require.True(t, p.Zoom)
+	require.True(t, p.Kakao)
+	require.True(t, p.Naver)
 }
 
 func TestSettings_EmailDisabled(t *testing.T) {
